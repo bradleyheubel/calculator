0f98c17d81f917b2ae98b5b98bfbf87f8ae73371
Bradley Heubel <bradley.heubel@education.vic.gov.au>
2020-11-18 21:20:20 +1100

    Added tabs and start of currency converter

---
+++ calculator/CurrencyView.swift
@@ -0,0 +1,22 @@
+//
+//  CurrencyView.swift
+//  calculator
+//
+//  Created by Bradley on 18/11/20.
+//
+
+import SwiftUI
+
+struct CurrencyView: View {
+    @State private var selected = 0
+    
+    var body: some View {
+            Text("Currency Tab")
+    }
+}
+
+struct CurrencyView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrencyView()
+    }
+}
